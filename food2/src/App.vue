e8d63fd5a4ef5d3c44ba455e5811feb6e7968062
zhenghan <sypp123456@gmail.com>
2022-02-23 08:51:03 +0800

    new version test

--- food2/src/App.vue
+++ food2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/OrderFood">點餐</router-link> |
+      <router-link to="/OrderFood">點餐1</router-link> |
       <router-link to="/SetFood">設定餐點</router-link> |
       <router-link to="/FoodSetting">食物設定</router-link> |
       <router-link to="/UsersSetting">人員設定</router-link>
--- food2/vue.config.js
+++ food2/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/zhenghanli.github.io/food2/'
+  : '/',
+  transpileDependencies: true,
+  publicPath:'./' 
 })
